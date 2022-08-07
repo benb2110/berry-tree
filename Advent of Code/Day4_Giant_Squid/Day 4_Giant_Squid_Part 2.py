9ffe68dcb9845aed1eb4bdbc7dd8344ac6a36f6f
benjaminberesford <benb2110@gmail.com>
2022-08-07 11:04:19 +1000

    Finished advent of code day 4 part 2

--- Advent of Code/Day4_Giant_Squid/Day 4_Giant_Squid_Part 2.py
+++ Advent of Code/Day4_Giant_Squid/Day 4_Giant_Squid_Part 2.py
@@ -34,3 +34,45 @@ test_boards = [[[22, 13, 17, 11,  0], [8,  2, 23,  4, 24], [21,  9, 14, 16,  7],
 [22, 11, 13,  6,  5],
  [2,  0, 12,  3, 7]]]
 
+def winning_board(boards): #Checks the boards for horizontal win or vertical, then returns the winning board
+    for k in range(len(boards)):
+        for i in range(5):
+            vert = []
+            if boards[k][i] == ['X', 'X', 'X', 'X', 'X']:  #Horizontal victory
+                print("Hor: board[" + str(k) + "] is the winning board")
+                return k
+            for j in range(5):
+                vert.append(boards[k][j][i])
+            if vert == ['X', 'X', 'X', 'X', 'X']: #Checks Vertical Victory
+                print("Vert: board[" + str(k) + "] is the winning board")
+                return k
+    return None
+
+def number_draw(draw, boards): #Checks each spot on the boards for the draw number, and then checks for a win, and then checks the score
+    for i in range(len(draw)):
+        for j in range(len(boards)):
+            for h in range(5):
+                for k in range(5):
+                    if draw[i] == boards[j][h][k]:
+                        boards[j][h][k] = 'X'
+                        winner = winning_board(boards)
+                        if winner is not None:
+                            if len(boards) >= 2:
+                                new_boards = boards
+                                new_boards.remove(new_boards[winner])
+                                new_nums = draw[i:]
+                                return number_draw(new_nums, new_boards) #testing recursion
+                            else:  #find score of last board
+                                winning_number = draw[i]
+                                score = 0
+                                for k in range(5):
+                                    for h in range(5):
+                                        if boards[winner][h][k] == 'X':
+                                            continue
+                                        else:
+                                            score += boards[winner][h][k]
+                                score = score * winning_number
+                                return score
+
+#print(number_draw(test_data, test_boards))
+print(number_draw(draw, board))
