9cf1577c38bd041c1eac4203d33508d27b2a3116
Ben Beresford <benb2110@gmail.com>
2022-08-11 10:32:46 +1000

    advent of code day 7 complete

--- Advent of Code/Day7_The_Treachery_of_Whales/Day 7_The Treachery_of_Whales_Part 2.py
+++ Advent of Code/Day7_The_Treachery_of_Whales/Day 7_The Treachery_of_Whales_Part 2.py
@@ -3,9 +3,10 @@ crab_pos_data = []
 l = open('Crab_Positions.txt').read().strip().split(',')
 for i in l:
     crab_pos_data.append(int(i))
+#Close txt and delete l
 
 
-def innefficient_fuel(n, n2):
+def fuel_consumption(n, n2):
     difference = abs(n - n2)
     fuel = 0
     for i in range(difference+1):
@@ -16,20 +17,17 @@ def innefficient_fuel(n, n2):
 def fuel_efficiency(data):
     fuel_ratings = []
     temp = 0
-    for i in range(len(data)):
+    for i in range(max(data)):
         fuel_ratings.append(temp)
         temp = 0
         for j in range(len(data)):
-            if data[i] == data[j]:
+            if data[j] == i:
                 continue
-            temp += innefficient_fuel(data[i], data[j])
-    print(fuel_ratings)
+            temp += fuel_consumption(data[j], i)
+
     return min(fuel_ratings[1:])
 
 
-print(fuel_efficiency(test_data))
-
-
-#print(innefficient_fuel(0, 3))
+print(fuel_efficiency(crab_pos_data))
 
 
