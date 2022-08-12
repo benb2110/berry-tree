82189338a4a2bbad932cef741854f20ae74c8b65
Ben Beresford <benb2110@gmail.com>
2022-08-12 18:49:02 +1000

    push

--- Advent of Code/Day8_Seven_Segment_Search/Day 8_Seven_Segment_Search_Part 1.py
+++ Advent of Code/Day8_Seven_Segment_Search/Day 8_Seven_Segment_Search_Part 1.py
@@ -23,10 +23,8 @@ def count_1_4_7_8(data): #counts instances of 1s, 4s, 7s and 8s
     d = [data[1::2]]
     for i in range(len(d[0])):
         for h in range(5):
-            print(d[0][i][h])
             if len(d[0][i][h]) == 2 or len(d[0][i][h]) == 3 or len(d[0][i][h]) == 4 or len(d[0][i][h]) == 7:
                 counter += 1
-                print(True)
     return counter
 
 
