84e124c356fac8585b3a14676f4a0a30ec07d13c
benjaminberesford <benb2110@gmail.com>
2022-08-14 13:11:10 +1000

    Seven Segment Displays update

--- Advent of Code/Day8_Seven_Segment_Search/Day 8_Seven_Segment_Search_Part 2.py
+++ Advent of Code/Day8_Seven_Segment_Search/Day 8_Seven_Segment_Search_Part 2.py
@@ -1,19 +1,60 @@
-zero = 'abcefg'
-one = 'cf'
-two = 'acdeg'
-three = 'acdfg'
-four = 'bcdf'
-five = 'abdfg'
-six = 'abdefg'
-seven = 'acf'
-eight = 'abcdefg'
-nine = 'abcdfg'
+    #                0           2         3          5          6          9
+#  signatures = [[2, 3, 3], [1, 2, 2], [2, 3, 3], [1, 3, 2], [1, 3, 2], [2, 4, 3]]
+
+#Method
+#format the string
+#find the known numbers
+#Determine the 6 letter digits by comparing them with known numbers(0, 2, 5, )
+#Determine the 5 letter digits by comparing them with known numbers(0, 2, 5, )
+
+t4 = 'egbfac', 'ceg', 'geacd', 'ecbda', 'ebgd', 'dbceag', 'dbgcfea', 'dbacef', 'gadfc', 'ge', 'egc', 'dcbaegf', 'acfgd', 'aegbdc'
+t3= 'faegd', 'eb', 'fabe', 'efbdag', 'egb', 'fgbed', 'cbgdea', 'dcfgb', 'cbgefad', 'gfecad', 'adbgcef', 'agfed', 'ebg', 'fbegda'
+t2 = 'fegbac', 'debg', 'bgc', 'cadbg', 'gdefca', 'efbagcd', 'dbgcea', 'dfbca', 'caegd', 'bg'
+#t = 'adgef', 'bagcf', 'cdea', 'adfebg', 'dfgca', 'adfgec', 'cd', 'fcdgbe', 'cdf', 'cdfegab', 'afgdbe', 'facdg', 'gdfbeac', 'dcea'
+#test_data = 'ec', 'cabfe', 'afebd', 'dbagef', 'afbcg', 'feabcd', 'cdef', 'eafdcbg', 'ecb', 'caegdb'
+numbers = ['', '', '', '', '', '', '', '', '', '']
+#knowns = ['ec', 'cdef', 'ecb', 'eafdcbg']
 
 
-def known_numbers(): # outputs known numbers (1, 4, 7, 8) into a string
-    pass
+def identify_unknowns(data):
+    numbers = ['', '', '', '', '', '', '', '', '', '']
+    knowns = []
+    for i in range(len(data)):
+        if len(data[i]) == 2:
+            numbers[1] = data[i]
+        if len(data[i]) == 3:
+            numbers[7] = data[i]
+        if len(data[i]) == 4:
+            numbers[4] = data[i]
+        if len(data[i]) == 7:
+            numbers[8] = data[i]
+    knowns = [numbers[1], numbers[4], numbers[7], numbers[8]]
+    for i in range(len(data)):
+        if signature_checker(data[i], knowns) == [6, 7]:
+            numbers[0] = data[i]
+        if signature_checker(data[i], knowns) == [6, 6]:
+            numbers[6] = data[i]
+        if signature_checker(data[i], knowns) == [6, 9]:
+            numbers[9] = data[i]
+        if signature_checker(data[i], knowns) == [5, 5]:
+            numbers[2] = data[i]
+        if signature_checker(data[i], knowns) == [5, 8]:
+            numbers[3] = data[i]
+        if signature_checker(data[i], knowns) == [5, 6]:
+            numbers[5] = data[i]
+    return(numbers)
 
 
-def unknown_numbers(): # outputs unknown numbers into a string
-    pass
+def signature_checker(n, knowns): #Each digit shares a unique number of segments with our four known numbers. Checking this means we can identify the numbers
+    sig = [len(n), 0]
+    for i in range(len(n)):
+        for j in range(3):
+            if n[i] in knowns[j]:
+                sig[1] += 1
+    return sig
+
+
+#print(signature_checker('feabcd', knowns))
+print(identify_unknowns(t4))
+
 
--- Advent of Code/Day8_Seven_Segment_Search/Messing_around_with_seven_segments.py
+++ Advent of Code/Day8_Seven_Segment_Search/Messing_around_with_seven_segments.py
@@ -5,65 +5,74 @@
 #  g
 
 #This approach involves comparing the segments in each unknown number against our four known numbers (1, 4, 7, 8). if the segment is shared
-# we get append True into its signature.
+# we append True into its signature.
 # It turns out each unknown number has a unique combination of shared segments with numbers (1, 4, 7)
 
     #6 letters
-#zero   2, 3, 3   #Zero and Nine shares two 2 segments with One, but six uniquely shares 1
-#six    1, 3, 2
-#nine   2, 4, 3
+#zero   2, 3, 3  =7 #Zero and Nine shares two 2 segments with One, but six uniquely shares 1
+#six    1, 3, 2  =6
+#nine   2, 4, 3  =9
 
     #5 letters
-#two    1, 2, 2
-#three  2, 3, 3
-#five   1, 3, 2
+#two    1, 2, 2  =5
+#three  2, 3, 3  =8
+#five   1, 3, 2  =6
 
 #Now that we know the signatures for our unknown numbers we can compare them with the knowns and output the correct ones
 
+test_data = 'ec', 'cabfe', 'afebd', 'dbagef', 'afbcg', 'feabcd', 'cdef', 'eafdcbg', 'ecb', 'caegdb',
 
+            #    0       1      2        3        4       5        6        7        8          9
 #segments = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg']
 
-unknowns = ['abcefg', 'acdeg', 'acdfg', 'abdfg', 'abdefg', 'abcdfg']
+unknowns = ['abcefg', 'acdeg', 'acdfg', 'abdfg', 'abdefg', 'abcdfg', '']
 knowns = ['cf', 'bcdf', 'acf', 'abcdefg']
 
-zero = 'abcefg'
-one = 'cf'
-two = 'acdeg'
-three = 'acdfg'
-four = 'bcdf'
-five = 'abdfg'
-six = 'abdefg'
-seven = 'acf'
-eight = 'abcdefg'
-nine = 'abcdfg'
+
+numbers = ['', '', '', '', '', '', '', '', '', '']
 
 
-def in_common2(string):
-    sig = [],[],[],[],[],[]
+
+def known_numbers(segs):
+    kn = ['', '', '', '', '', '', '', '', '', '']
+    for i in range(len(segs)):
+        if len(segs[i]) == 2:
+            kn[1] = segs[i]
+        if len(segs[i]) == 3:
+            kn[7] = segs[i]
+        if len(segs[i]) == 4:
+            kn[4] = segs[i]
+        if len(segs[i]) == 7:
+            kn[8] = segs[i]
+    return kn
+
+
+def in_common(string):
+    sig = []
+    temp = None
     for i in range(len(string)):
+        sig.append(temp)
+        temp = [0, 0, 0]
         for k in range(3):
-            temp = []
-            sig[i].append(temp)
             for j in range(len(string[i])):
                 if string[i][j] in knowns[k]:
-                    temp.append(True)
-    return sig
+                    temp[k] += 1
+    return sig[1:]
+
+
+def identify_unknowns(data):
+    numbers = ['', '', '', '', '', '', '', '', '', '']
+    knowns = [numbers[1], numbers[4], numbers[7], numbers[8]]
 
 
 
-def in_common2(string):
-    sig = [],[],[],[],[],[]
-    for i in range(len(string)):
-        for k in range(3):
-            temp = []
-            sig[i].append(temp)
-            for j in range(len(string[i])):
-                if string[i][j] in knowns[k]:
-                    temp.append(True)
-    return sig
-
-
-signatures = in_common2(unknowns)
-
+signatures = in_common(unknowns)
 print(signatures)
+
+#numbers = known_numbers(test_data)
+#print(numbers)
+
+kn = [numbers[1], numbers[4], numbers[7], numbers[8]]
+print(kn)
+
 
