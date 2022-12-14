eefb661079c39e6bd46b7ef15890a97d2f5c5eef
Ben Beresford <benb2110@gmail.com>
2022-12-14 12:29:21 +1100

    update

---
+++ Advent of Code/2022/Day 4_Camp_Cleanup/Camp_Cleanup_Part_1.py
@@ -0,0 +1,24 @@
+duties = []
+with open('Test Data.txt') as d: #importing data
+    for line in d:
+        duties.append(line.strip())
+d.close()
+
+result = [x.split(',') for x in duties]
+print(result)
+
+def overlap_counter(data):
+    count = 0
+    for i in range(len(result)):
+        if result[i][0][0] <= result[i][1][0] and result[i][0][2] >= result[i][1][2]:
+            count += 1
+        elif result[i][0][0] >= result[i][1][0] and result[i][0][2] <= result[i][1][2]:
+            count += 1
+    return count
+
+print(result[0][0])
+print(result[0][1])
+#print(overlap_counter(result))
+
+#if a =< x and b =>y or a =>x and b=<y
+
