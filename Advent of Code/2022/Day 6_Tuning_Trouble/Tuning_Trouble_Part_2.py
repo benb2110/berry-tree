52463b4cce09e4833985945095dbc6d89e14fc2d
Ben Beresford <benb2110@gmail.com>
2023-01-08 12:37:27 +1100

    AOC Day 6 Complete

---
+++ Advent of Code/2022/Day 6_Tuning_Trouble/Tuning_Trouble_Part_2.py
@@ -0,0 +1,36 @@
+data = []
+
+
+with open('Data.txt') as d: #importing data
+    for line in d:
+        data.append(str(line))
+d.close()
+
+data = data[0]
+
+
+
+def unique_string(string):
+    flag_count = 0
+    if len(string) == 14:
+        for i in range(14):
+            for j in range(14):
+                if string[j-1] == string[i-1]:
+                    flag_count += 1
+    else:
+        return False
+
+    if flag_count == 14:
+        return True
+    else:
+        return False
+
+
+def signal_start(signal):
+    packet_markers = []
+    for i in range(len(signal)):
+        if unique_string(signal[i-14:i]):
+            packet_markers.append(i)
+    return packet_markers
+
+print(signal_start(data))
