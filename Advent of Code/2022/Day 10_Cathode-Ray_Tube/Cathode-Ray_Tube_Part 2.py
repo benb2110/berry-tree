4872a8b35f82e1f72d46b6edb16ee5c3aa8141b0
benjaminberesford <benb2110@gmail.com>
2023-04-29 13:07:44 +1000

    Advent of Code Day 10 Part 1 Complete

---
+++ Advent of Code/2022/Day 10_Cathode-Ray_Tube/Cathode-Ray_Tube_Part 2.py
@@ -0,0 +1,35 @@
+instructions = []
+
+with open('Data.txt') as d: #importing data
+    for line in d:
+        instructions.append(line.split())
+d.close()
+
+signal_sums = 0
+ref = 20
+
+
+def signal_check(X, cycle):
+    global signal_sums, ref
+    if cycle == ref:
+        print(cycle * X)
+        signal_sums += cycle * X
+        ref += 40
+
+
+def operations(inputs):
+    x = 1
+    cycle = 0
+    for i in range(len(inputs)):
+        if str(inputs[i][0]) == 'addx':
+            for loop in range(2):
+                cycle += 1
+                signal_check(x, cycle)
+            x += int(inputs[i][1])
+        else:
+            cycle += 1
+            signal_check(x, cycle)
+
+
+operations(instructions)
+print(signal_sums)
