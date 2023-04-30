f6e511023b7b9f13edd0f53c22a6c48a85027c5f
benjaminberesford <benb2110@gmail.com>
2023-04-30 10:45:35 +1000

    AOC Day 10 Part 2 init

--- Advent of Code/2022/Day 10_Cathode-Ray_Tube/Cathode-Ray_Tube_Part 2.py
+++ Advent of Code/2022/Day 10_Cathode-Ray_Tube/Cathode-Ray_Tube_Part 2.py
@@ -1,6 +1,6 @@
 instructions = []
 
-with open('Data.txt') as d: #importing data
+with open('Test Data.txt') as d: #importing data
     for line in d:
         instructions.append(line.split())
 d.close()
@@ -8,28 +8,52 @@ d.close()
 signal_sums = 0
 ref = 20
 
+sprite_pos = ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
 
-def signal_check(X, cycle):
-    global signal_sums, ref
-    if cycle == ref:
-        print(cycle * X)
-        signal_sums += cycle * X
-        ref += 40
+#init CRT display
+CRT = []
+for l in range(6):
+    line = []
+    for i in range(40):
+        line.append('.')
+    CRT.append(line)
+print(CRT)
+
+
+def sprite_update(x):
+    global sprite_pos, CRT
+    sprite_pos = ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
+    sprite_pos[x] = '#'
+    sprite_pos[x-1] = '#'
+    sprite_pos[x+1] = '#'
+
+
+def draw_cycle(step, cycle):
+    global sprite_pos, CRT
+    if sprite_pos[cycle] == '#':
+        CRT[step][cycle] = '#'
 
 
 def operations(inputs):
     x = 1
     cycle = 0
+    step = 0
     for i in range(len(inputs)):
+        if cycle == 39:
+            step += 1
+            cycle -= 39
+        sprite_update(x)    #update sprite position
         if str(inputs[i][0]) == 'addx':
             for loop in range(2):
+                sprite_update(x) #possibly redundant
+                draw_cycle(step, cycle)   #draw sprite
                 cycle += 1
-                signal_check(x, cycle)
             x += int(inputs[i][1])
         else:
+            draw_cycle(step, cycle)     #draw sprite
             cycle += 1
-            signal_check(x, cycle)
+
 
 
 operations(instructions)
-print(signal_sums)
+#print(signal_sums)
